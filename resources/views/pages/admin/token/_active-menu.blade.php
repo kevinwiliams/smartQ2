4f5f6dc7a4df7c149cc0cb03939594645d25758c
Kevin Williams <kwilliams9285@gmail.com>
2022-03-04 00:16:48 -0500

    DT Solution

--- resources/views/pages/admin/token/_active-menu.blade.php
+++ resources/views/pages/admin/token/_active-menu.blade.php
@@ -1,22 +1,45 @@
-<div class="d-flex justify-content-end flex-shrink-0">
-    <a href="#" class="btn btn-icon btn-bg-light btn-active-color-success btn-sm me-1">
-        <!--begin::Svg Icon | path: icons/duotune/general/gen019.svg-->
-        {!! theme()->getSvgIcon("icons/duotune/general/gen043.svg", "svg-icon-1") !!}
-        <!--end::Svg Icon-->
-    </a>
-    <a href="#" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1">
-        <!--begin::Svg Icon | path: icons/duotune/general/gen019.svg-->
-        {!! theme()->getSvgIcon("icons/duotune/general/gen016.svg", "svg-icon-2") !!}
-        <!--end::Svg Icon-->
-    </a>
-    <a href="#" class="btn btn-icon btn-bg-light btn-active-color-warning btn-sm me-1">
-        <!--begin::Svg Icon | path: icons/duotune/art/art005.svg-->
-        {!! theme()->getSvgIcon("icons/duotune/abstract/abs051.svg", "svg-icon-2") !!}
-        <!--end::Svg Icon-->
-    </a>
-    {{-- <a href="#" class="btn btn-icon btn-bg-light btn-active-color-danger btn-sm">
-        <!--begin::Svg Icon | path: icons/duotune/general/gen027.svg-->
-        {!! theme()->getSvgIcon("icons/duotune/general/gen027.svg", "svg-icon-1") !!}
-        <!--end::Svg Icon-->
-    </a> --}}
+<a href="#" class="btn btn-light btn-active-light-primary btn-sm" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end" data-kt-menu-flip="top-end">
+    Actions
+    <!--begin::Svg Icon | path: icons/duotune/arrows/arr072.svg-->
+    {!! theme()->getSvgIcon("icons/duotune/arrows/arr072.svg", "svg-icon-5 m-0") !!}
+    <!--end::Svg Icon-->
+</a>
+<!--begin::Menu-->
+<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4" data-kt-menu="true">
+    <!--begin::Menu item-->
+    <div class="menu-item px-3">
+        <a href="#" class="menu-link px-3" data-kt-token-table-filter="edit_row">
+            Complete
+        </a>
+    </div>
+    <!--end::Menu item-->
+    <!--begin::Menu item-->
+    <div class="menu-item px-3">
+        <a href="#" class="menu-link px-3" data-kt-token-table-filter="edit_row">
+            Transfer
+        </a>
+    </div>
+    <!--end::Menu item-->
+    <!--begin::Menu item-->
+    <div class="menu-item px-3">
+        <a href="#" class="menu-link px-3" data-kt-token-table-filter="edit_row">
+            Cancel
+        </a>
+    </div>
+    <!--end::Menu item-->
+    <!--begin::Menu item-->
+    <div class="menu-item px-3">
+        <a href="#" class="menu-link px-3" data-kt-token-table-filter="edit_row">
+            Print
+        </a>
+    </div>
+    <!--end::Menu item-->
+
+    <!--begin::Menu item-->
+    <div class="menu-item px-3">
+        <a href="#" class="menu-link px-3" data-kt-token-table-filter="delete_row">
+            Delete
+        </a>
+    </div>
+    <!--end::Menu item-->
 </div>
--- resources/views/pages/admin/token/current.blade.php
+++ resources/views/pages/admin/token/current.blade.php
@@ -8,13 +8,13 @@
                 <span class="text-muted mt-1 fw-bold fs-7">Clients waiting: {{ $waiting }}</span> --}}
                 <div class="d-flex align-items-center position-relative my-1 me-5">
                     <!--begin::Svg Icon | path: icons/duotune/general/gen021.svg-->
-                    {!! theme()->getSvgIcon("icons/duotune/general/gen021.svg", "svg-icon-1") !!}
+                    {!! theme()->getSvgIcon("icons/duotune/general/gen021.svg", "svg-icon-1 position-absolute ms-6") !!}
                     <!--end::Svg Icon-->
                     <input type="text" data-kt-token-table-filter="search" class="form-control form-control-solid w-250px ps-15" placeholder="Search Active Tokens">
                 </div>
             </h3>
             <div class="card-toolbar" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover" title="" data-bs-original-title="Click create a new token">
-                <a href="#" class="btn btn-sm btn-light btn-active-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_add_token">
+                <a href="#" class="btn btn-sm btn-light-primary btn-active-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_add_token">
                 <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
                 {!! theme()->getSvgIcon("icons/duotune/arrows/arr075.svg", "svg-icon-3") !!}
                 <!--end::Svg Icon-->New Token</a>
@@ -30,126 +30,33 @@
         <!--end::Card body-->
     </div>
     <!--end::Card-->   
-    
-    <!--begin::Modal - Add task-->
-		<div class="modal fade" id="kt_modal_add_token" tabindex="-1" aria-hidden="true">
-			<!--begin::Modal dialog-->
-			<div class="modal-dialog modal-dialog-centered mw-650px">
-				<!--begin::Modal content-->
-				<div class="modal-content">
-					<!--begin::Modal header-->
-					<div class="modal-header">
-						<!--begin::Modal title-->
-						<h2 class="fw-bolder">Create new token</h2>
-						<!--end::Modal title-->
-						<!--begin::Close-->
-						<div class="btn btn-icon btn-sm btn-active-icon-primary" data-kt-tokens-modal-action="close">
-							<!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
-							{!! theme()->getSvgIcon("icons/duotune/arrows/arr061.svg", "svg-icon-1") !!}
-							<!--end::Svg Icon-->
-						</div>
-						<!--end::Close-->
-					</div>
-					<!--end::Modal header-->
-					<!--begin::Modal body-->
-					<div class="modal-body scroll-y mx-5 mx-xl-15 my-5">
-                    <div id="output" class="hide alert alert-danger alert-dismissible fade in shadowed mb-1"></div>
-						<!--begin::Form-->
-                        {{ Form::open(['url' => 'admin/token/create', 'class'=>'manualFrm form', 'id'=>'kt_modal_add_token_form']) }}
-                        @csrf <!-- {{ csrf_field() }} -->
-							<!--begin::Input group-->
-							<div class="fv-row mb-7">
-                                {{-- @if($display->sms_alert) --}}
-                                <div class="form-group @error('client_mobile') has-error @enderror">
-                                    <label class="required fs-6 fw-bold form-label mb-2" for="client_mobile">{{ trans('app.client_mobile') }} </label>
-                                    <input type="text" name="client_mobile" class="form-control form-control-solid" placeholder="{{ trans('app.client_mobile') }}"/>  
-                                    <span class="text-danger">{{ $errors->first('client_mobile') }}</span>
-                                </div>   
-                                {{-- @endif --}}
-							</div>
-							<!--end::Input group-->
-							<!--begin::Input group-->
-							<div class="fv-row mb-7">
-								<div class="form-group @error('department_id') has-error @enderror">
-                                    <label class="fs-6 fw-bold form-label mb-2" for="department_id"><span class="required">{{ trans('app.department') }}</span>
-                                    <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="popover" data-bs-trigger="hover" data-bs-html="true" data-bs-content="Please assign the correct department"></i></label>
-                                    {{ Form::select('department_id', $departments, null, ['data-placeholder' => 'Select Option','placeholder' => 'Select Option', 'data-control' => 'select2' , 'class'=>'form-select form-select-solid form-select-lg fw-bold']) }}
-                                    <span class="text-danger">{{ $errors->first('department_id') }}</span>
-                                </div> 
-							</div>
-							<!--end::Input group-->
-                            <!--begin::Input group-->
-							<div class="fv-row mb-7">
-								<div class="form-group @error('counter_id') has-error @enderror">
-                                    <label class="fs-6 fw-bold form-label mb-2" for="counter_id"><span class="required">{{ trans('app.counter') }}</span>
-                                    <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="popover" data-bs-trigger="hover" data-bs-html="true" data-bs-content="Assign user to a counter"></i></label>
-                                    {{ Form::select('counter_id', $counters, null, ['data-placeholder' => 'Select Option','placeholder' => 'Select Option', 'data-control' => 'select2' , 'class'=>'form-select form-select-solid form-select-lg fw-bold']) }}
-                                    <span class="text-danger">{{ $errors->first('counter_id') }}</span>
-                                </div> 
-							</div>
-							<!--end::Input group-->
-                            <!--begin::Input group-->
-							<div class="fv-row mb-7">
-								<div class="form-group @error('user_id') has-error @enderror">
-                                    <label class="fs-6 fw-bold form-label mb-2" for="user_id"><span class="required">{{ trans('app.officer') }}</span>
-                                    <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="popover" data-bs-trigger="hover" data-bs-html="true" data-bs-content="Assign user to a officer"></i></label>
-                                    {{ Form::select('user_id', $officers, null, ['data-placeholder' => 'Select Option','placeholder' => 'Select Option', 'data-control' => 'select2' , 'class'=>'form-select form-select-solid form-select-lg fw-bold']) }}
-                                    <span class="text-danger">{{ $errors->first('user_id') }}</span>
-                                </div> 
-							</div>
-							<!--end::Input group-->
-							<!--begin::Input group-->
-							<div class="fv-row mb-7">
-                            {{-- @if($display->show_note) --}}
-                                <div class="form-group @error('note') has-error @enderror">
-                                    <label class="fs-6 fw-bold form-label mb-2" for="note">{{ trans('app.note') }}</label> 
-                                    <textarea name="note" id="note" class="form-control form-control-solid rounded-3"  placeholder="{{ trans('app.note') }}">{{ old('note') }}</textarea>
-                                    <span class="text-danger">{{ $errors->first('note') }}</span> 
-                                </div>
-                            {{-- @endif --}}
-							</div>
-							<!--end::Input group-->
-                            <!--begin::Input group-->
-							<div class="fv-row mb-7">
-                                <label class="form-check form-switch form-check-custom form-check-solid">
-                                    <input class="form-check-input" type="checkbox" value="1" name="is_vip">
-                                    <span class="form-check-label fw-bold"> {{ trans('app.is_vip') }}</span>
-                                </label>
-                            </div>
-                            <!--end::Input group-->
-							<!--begin::Actions-->
-							<div class="text-center pt-15">
-								<button type="reset" class="btn btn-light me-3" data-kt-tokens-modal-action="cancel">Discard</button>
-								<button type="submit" class="btn btn-primary" data-kt-tokens-modal-action="submit" >
-									<span class="indicator-label">Submit</span>
-									<span class="indicator-progress">Please wait...
-									<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-								</button>
-							</div>
-							<!--end::Actions-->
-                        {{ Form::close() }}
-						<!--end::Form-->
-					</div>
-					<!--end::Modal body-->
-				</div>
-				<!--end::Modal content-->
-			</div>
-			<!--end::Modal dialog-->
-		</div>
-		<!--end::Modal - Add task-->
+    <!--begin::Modal - Add Token -->
+    {{ theme()->getView('partials/modals/token/_add', 
+        array(
+            'officers' => $officers, 
+            'counters' => $counters, 
+            'departments' => $departments
+            )) }}
+    <!--end::Modal - Add Token-->
+
     {{-- Inject Scripts --}}
 @section('scripts')
     {{ $dataTable->scripts() }}
-    <script>
-        $(document).ready(function() {
-            //$('#token-table').DataTable();
-            alert('ready');
-        } );
-    </script>
+
+<script>
+    $(document).ready(function() { //required to fire menu on dt
+        var table = $('#token-table').DataTable();
+        table.on('draw', function () {
+                KTMenu.createInstances(); //load action menu options
+            });
+        } ); 
+    //search bar    
+    const filterSearch = document.querySelector('[data-kt-token-table-filter="search"]');
+    filterSearch.addEventListener('keyup', function (e) {
+        var table = $('#token-table').DataTable();
+        table.search(e.target.value).draw();
+    });
+    
+</script>
 @endsection
-@push('scripts')
-
-@endpush
- 
-
 </x-base-layout>
