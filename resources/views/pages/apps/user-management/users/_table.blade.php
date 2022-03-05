7ca7b2fdf6e365bfc81e6694345de765febb5f81
Grimm876 <gdickson@tumpit.com>
2022-03-05 16:52:55 -0500

    user table update

--- resources/views/pages/apps/user-management/users/_table.blade.php
+++ resources/views/pages/apps/user-management/users/_table.blade.php
@@ -12,9 +12,9 @@
         
         table.on('draw', function() {
             KTMenu.createInstances(); //load action menu options
-            const deleteButtons = table.querySelectorAll('[data-kt-users-table-filter="delete_row"]');
-            console.log('delete');
-            console.log(deleteButtons);
+            // const deleteButtons = table.querySelectorAll('[data-kt-users-table-filter="delete_row"]');
+            // console.log('delete');
+            // console.log(deleteButtons);
         }); 
     });
     //search bar    
