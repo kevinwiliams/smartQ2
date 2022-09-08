da395ebf0a4542cb9fd69c8920de423c4ae4840b
Grimm876 <gdickson@tumpit.com>
2022-09-08 16:37:09 -0500

    remove api keys

--- config/larafirebase.php
+++ config/larafirebase.php
@@ -2,8 +2,8 @@
 
 return [
 
-    'authentication_key' => 'AAAABnl-FB8:APA91bE5cCOTp5z4_5Jqp_eOJtuYDf22JFqEyBJ96JpYUpj7gBYbiJ5gYkUpxCEcCIxXBDyRoODVi-r_SY5X6SKjyRPLBBMYna9DF-g4j90H-mIi1VkF0Ol65YBIPEaGjeeLvNdcr6Y9',
-    'api_key' => 'AIzaSyA5Z0VuHZovrlMu789_MUU8nvLYegltj1g',
+    'authentication_key' => '',
+    'api_key' => '',
     'messagingSenderId' => '27808109599',
     'projectId' => 'smartq-340122',
     'appId' => '1:27808109599:web:58f55608b16d0b6b33b12a',
@@ -12,13 +12,3 @@ return [
     'storageBucket' => 'smartq-340122.appspot.com'
 
 ];
-
-
-
-// apiKey: "AIzaSyA5Z0VuHZovrlMu789_MUU8nvLYegltj1g",
-// authDomain: "smartq-340122.firebaseapp.com",
-// projectId: "smartq-340122",
-// storageBucket: "smartq-340122.appspot.com",
-// messagingSenderId: "27808109599",
-// appId: "1:27808109599:web:58f55608b16d0b6b33b12a",
-// measurementId: "G-8C6E8MQGC8"
