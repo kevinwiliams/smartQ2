e6971d9c7d3191cc663e64b37b727ea4598813c1
Grimm876 <gdickson@tumpit.com>
2023-07-04 21:44:46 -0500

    no titles

--- config/global/pages.php
+++ config/global/pages.php
@@ -520,38 +520,38 @@ return array(
             )
         ),
     ),
-    'home' => array(
-        'title' => 'Home',
-        'home' => array(
-            '*' => array(
-                'title'  => 'Home',                
-            )
-        ),
-        'search' => array(
-            '*' => array(
-                'title'  => 'Join the Queue',                
-            )
-        ),
-        'current' => array(
-            '*' => array(
-                'title'  => 'Your Token',                
-            )
-        ),
-        // 'search' => array(
-        //     '*' => array(
-        //         'title'  => 'Location List',                
-        //     )
-        // ),
+    // 'home' => array(
+    //     'title' => 'Home',
+    //     'home' => array(
+    //         '*' => array(
+    //             'title'  => 'Home',                
+    //         )
+    //     ),
+    //     'search' => array(
+    //         '*' => array(
+    //             'title'  => 'Join the Queue',                
+    //         )
+    //     ),
+    //     'current' => array(
+    //         '*' => array(
+    //             'title'  => 'Your Token',                
+    //         )
+    //     ),
+    //     // 'search' => array(
+    //     //     '*' => array(
+    //     //         'title'  => 'Location List',                
+    //     //     )
+    //     // ),
       
-    ),
-     'in' => array(
-        'title' => 'Search',
+    // ),
+    //  'in' => array(
+    //     'title' => 'Search',
 
-        '*' => array(
-            'title' => 'Search',
-        ),
+    //     '*' => array(
+    //         'title' => 'Search',
+    //     ),
 
-    ),
+    // ),
     // 'settings' => array(
     //     'title' => 'App Settings',
     //     'display' => array(
